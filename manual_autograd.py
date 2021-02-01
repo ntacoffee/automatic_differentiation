b8f6039120f7dd15fed66f60a1f5ad820716397d
nTaco <67748744+ntacoffee@users.noreply.github.com>
2021-02-01 21:28:44 +0900

    [update] modified variables' name.

--- manual_autograd.py
+++ manual_autograd.py
@@ -5,52 +5,52 @@
 import numpy as np
 
 x1 = 1.0
-x1_chain = {"requires_grad": True, "grad": 0, "backward_branches": []}
+x1_node = {"requires_grad": True, "grad": 0, "backward_branches": []}
 x2 = np.pi / 4
-x2_chain = {"requires_grad": True, "grad": 0, "backward_branches": []}
+x2_node = {"requires_grad": True, "grad": 0, "backward_branches": []}
 
 y1 = 2 * x1
-y1_chain = {
+y1_node = {
     "requires_grad": True,
     "grad": 0,
-    "backward_branches": [{"chain": x1_chain, "f_grad": 2}],
+    "backward_branches": [{"previous_node": x1_node, "f_grad": 2}],
 }
 y2 = x1 * np.sin(x2)
-y2_chain = {
+y2_node = {
     "requires_grad": True,
     "grad": 0,
     "backward_branches": [
-        {"chain": x1_chain, "f_grad": np.sin(x2)},
-        {"chain": x2_chain, "f_grad": x1 * np.cos(x2)},
+        {"previous_node": x1_node, "f_grad": np.sin(x2)},
+        {"previous_node": x2_node, "f_grad": x1 * np.cos(x2)},
     ],
 }
 
 z = y1 + y2
-z_chain = {
+z_node = {
     "requires_grad": True,
     "grad": 0,
     "backward_branches": [
-        {"chain": y1_chain, "f_grad": 1},
-        {"chain": y2_chain, "f_grad": 1},
+        {"previous_node": y1_node, "f_grad": 1},
+        {"previous_node": y2_node, "f_grad": 1},
     ],
 }
 
 
-def backward(chain, grad=1):
+def backward(node, grad=1):
 
-    if chain["requires_grad"]:
-        chain["grad"] += grad
+    if node["requires_grad"]:
+        node["grad"] += grad
 
-    if not chain["backward_branches"]:
+    if not node["backward_branches"]:
         # reached the end of the chain
         return
 
-    for branch in chain["backward_branches"]:
-        backward(branch["chain"], grad * branch["f_grad"])
+    for backward_branch in node["backward_branches"]:
+        backward(backward_branch["previous_node"], grad * backward_branch["f_grad"])
 
 
-backward(z_chain)
+backward(z_node)
 
 # 可視化
-print(x1_chain["grad"])
-print(x2_chain["grad"])
+print(x1_node["grad"])
+print(x2_node["grad"])
