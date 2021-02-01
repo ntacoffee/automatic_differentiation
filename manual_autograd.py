781c04f691baf28f0935e7de43fb4599d995f634
nTaco <67748744+ntacoffee@users.noreply.github.com>
2021-02-01 19:03:56 +0900

    [update] Reorganize the architecture for flexibility.
    z_chain can now be differentiated by y1, y2, z.

--- manual_autograd.py
+++ manual_autograd.py
@@ -5,35 +5,51 @@
 import numpy as np
 
 x1 = 1.0
-x1_chain = {"type": "param", "grad": 0}
+x1_chain = {"requires_grad": True, "grad": 0, "backward_branches": []}
 x2 = np.pi / 4
-x2_chain = {"type": "param", "grad": 0}
+x2_chain = {"requires_grad": True, "grad": 0, "backward_branches": []}
 
 y1 = 2 * x1
-y1_chain = {"type": "func", "upstream": [x1_chain], "grad": [2]}
+y1_chain = {
+    "requires_grad": True,
+    "grad": 0,
+    "backward_branches": [{"chain": x1_chain, "f_grad": 2}],
+}
 y2 = x1 * np.sin(x2)
 y2_chain = {
-    "type": "func",
-    "upstream": [x1_chain, x2_chain],
-    "grad": [np.sin(x2), x1 * np.cos(x2)],
+    "requires_grad": True,
+    "grad": 0,
+    "backward_branches": [
+        {"chain": x1_chain, "f_grad": np.sin(x2)},
+        {"chain": x2_chain, "f_grad": x1 * np.cos(x2)},
+    ],
 }
 
 z = y1 + y2
-z_chain = {"type": "func", "upstream": [y1_chain, y2_chain], "grad": [1, 1]}
+z_chain = {
+    "requires_grad": True,
+    "grad": 0,
+    "backward_branches": [
+        {"chain": y1_chain, "f_grad": 1},
+        {"chain": y2_chain, "f_grad": 1},
+    ],
+}
 
 
-def backward(chain, grad):
-    if chain["type"] == "param":
+def backward(chain, grad=1):
+
+    if chain["requires_grad"]:
         chain["grad"] += grad
-    elif chain["type"] == "func":
-        for i, upstream_chain in enumerate(chain["upstream"]):
-            backward(upstream_chain, grad * chain["grad"][i])
-    else:
-        # 本来はerrorを吐く
-        pass
+
+    if not chain["backward_branches"]:
+        # reached the end of the chain
+        return
+
+    for branch in chain["backward_branches"]:
+        backward(branch["chain"], grad * branch["f_grad"])
 
 
-backward(z_chain, 1)
+backward(z_chain)
 
 # 可視化
 print(x1_chain["grad"])
